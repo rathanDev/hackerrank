81eb39f16367f4a72f344106eb6a1756c7360a37
jana <rathan.kparam@gmail.com>
2017-05-17 00:00:20 +0530

    Added Dup3

--- src/main/java/jana/Duplication1.java
+++ src/main/java/jana/Duplication1.java
@@ -7,50 +7,9 @@ import java.util.Scanner;
  */
 public class Duplication1 {
 
-
-
     static String duplication(int x){
         // Complete this function
-        int index = x;
-
-
-        String binaryString = Integer.toBinaryString(index);
-        System.out.println("binaryString = " + binaryString);
-        int length = binaryString.length();
-        System.out.println("length = " + length);
-
-        String xInitial = "0";
-
-        StringBuilder s = new StringBuilder();
-
-        s.append(xInitial);
-        for (int i = 0; i <= length; i++) {
-
-            StringBuilder t = new StringBuilder();
-            for (char ch : s.toString().toCharArray()) {
-                t.append(ch == '0' ? '1' : '0');
-            }
-            s.append(t.toString());
-        }
-
-        System.out.println("x.toString() = " + s.toString());
-
-//        String x = "1001";
-
-
-        index = 2;
-        char c = s.charAt(index);
-        System.out.println("c = " + c);
-
-        index = 5;
-        c = s.charAt(index);
-        System.out.println("c = " + c);
-
-        index = 7;
-        c = s.charAt(index);
-        System.out.println("c = " + c);
-
-        return String.valueOf(s.charAt(index));
+        return "";
     }
 
     public static void main(String[] args) {
@@ -62,20 +21,5 @@ public class Duplication1 {
             System.out.println(result);
         }
     }
-
-//    public static void main(String[] args) {
-//        System.out.println("Hello World!");
-//
-//        //input     times   max
-//        //1     ->  1       2
-//        //2         1       2
-//        //
-//
-//        int input = 10;
-//
-//
-
-//    }
-
 
 }
--- src/main/java/jana/Duplication3.java
+++ src/main/java/jana/Duplication3.java
@@ -3,5 +3,59 @@
  */
 package jana;
 
+import java.util.Scanner;
+
 public class Duplication3 {
+
+    public static void main(String[] args) {
+        System.out.println("Dup 3");
+
+        System.out.println("Enter q");
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+
+        int biggestIndexEntered = -1;
+
+        String s = null;
+
+        for(int a0 = 0; a0 < q; a0++){
+
+            System.out.println();
+            System.out.println("Enter x");
+            int x = in.nextInt();
+
+            if (x > biggestIndexEntered) {
+                s = generateS(x);
+                biggestIndexEntered = x;
+            }
+
+            String result = String.valueOf(s.charAt(x));
+            System.out.println(result);
+        }
+    }
+
+    static String generateS(int index) {
+        String binaryString = Integer.toBinaryString(index);
+        System.out.println("binaryString = " + binaryString);
+        int noOfTimesToLoop = binaryString.length();
+        System.out.println("noOfTimesToLoop = " + noOfTimesToLoop);
+
+        String xInitial = "0";
+
+        StringBuilder s = new StringBuilder();
+
+        s.append(xInitial);
+        for (int i = 1; i <= noOfTimesToLoop; i++) {
+
+            StringBuilder t = new StringBuilder();
+            for (char ch : s.toString().toCharArray()) {
+                t.append(ch == '0' ? '1' : '0');
+            }
+            s.append(t.toString());
+        }
+
+        System.out.println("x.toString() = " + s.toString());
+        return s.toString();
+    }
+
 }
